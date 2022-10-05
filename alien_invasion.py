800e4a0f29b903ad036f212e2ef56734c2046703
“Cory <coryurton@gmail.com>
2022-10-04 19:27:26 -0500

    feat: background color

--- alien_invasion.py
+++ alien_invasion.py
@@ -10,19 +10,24 @@ class AlienInvasion:
         """Initialize the game and create game resources."""
         pygame.init()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode((1200, 800))  # surface
         pygame.display.set_caption("Alien Invasion")
+
+        # set the background color
+        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         """Start the main loop for the game."""
         while True:
-            # watch for key and mouse events
+            # listen for key and mouse events (event loop)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
 
+            # redraw the screen during each pass through the loop
+            self.screen.fill(self.bg_color)
 
-            # make the most recently drawn screen visible
+            # make the most recently drawn screen visible, illusory movement
             pygame.display.flip()
 
 
