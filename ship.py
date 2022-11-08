92812b7b9a576d9c1513e771737db0d504349291
“Cory <coryurton@gmail.com>
2022-11-07 23:47:38 -0600

    improved comments, additional feature to prevent ship falling off screen edges

--- ship.py
+++ ship.py
@@ -7,6 +7,7 @@ class Ship:
     def __init__(self, ai_game):
         """Initialize the ship and set its starting position."""
         self.screen = ai_game.screen
+
         # need a settings attribute to access in update()
         self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
@@ -25,24 +26,25 @@ class Ship:
         self.moving_right = False
         self.moving_left = False
 
-
-
     def update(self):
         """Update ship's position based on movement flag."""
-        # instead of its rect, update ship's x value based on movement flags
-        if self.moving_right:
+        # instead of its rect, update ship's x value instead of rect, based on
+        # movement flags
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            # above conditional to prevent ship going off the screen
             self.x += self.settings.ship_speed
-        #     updating self.x by the value of settings.ship_speed
-        if self.moving_left:
+            # updating self.x by the speed value in settings attr
+
+        if self.moving_left and self.rect.left > 0:
+            # above conditional to prevent ship going off the screen
             self.x -= self.settings.ship_speed
 
         # two separate if statements allow stand-still when both pressed
 
-        # update ship's self.rect.x after updating self.x
+        # update ship's self.rect.x (actual position) after updating self.x
+        # note: only integer portion being stored for now (bc rect)
         self.rect.x = self.x
 
     def blitme(self):
         """Draw the ship at its current location using its rect and blit()"""
         self.screen.blit(self.image, self.rect)
-
-
