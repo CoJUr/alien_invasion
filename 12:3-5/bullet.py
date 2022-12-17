5d6fe85113674b20dc5435af3aeb9aaf8f95c213
“Cory <coryurton@gmail.com>
2022-12-16 23:47:33 -0600

    remaining bullet.py impl

--- 12:3-5/bullet.py
+++ 12:3-5/bullet.py
@@ -13,9 +13,22 @@ class Bullet(Sprite):
         self.color = self.settings.bullet_color
 
         # Create a bullet rect at 0, 0 and then set position
-        self.rect = pygame.Rect(0, 0,
+        self.rect = pygame.Rect(0, 0,  # init placeholder position values
                                 self.settings.bullet_width, self.settings.bullet_height)
+        # set correct position now (bullet position depends on ship position)
+        # makes bullets fire from top of the ship
         self.rect.midtop = ai_game.ship.rect.midtop
 
-        # store the bullet position as a decimal
+        # store the bullet position as a decimal so can make fine bullet speed adjustments
         self.y = float(self.rect.y)
+
+    def update(self):
+        """Function for moving the bullet up the screen"""
+        # update decimal position of bullet
+        self.y -= self.settings.bullet_speed
+        # update rect position:
+        self.rect.y = self.y
+
+    def draw_bullet(self):
+        """Function to draw bullet on screen"""
+        pygame.draw.rect(self.screen, self.color, self.rect)
